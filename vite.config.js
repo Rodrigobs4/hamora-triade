e69c2587c0f77845d69346b973d995eeb6737395
Rodrigobs4 <rpdrogpbs4@gomail.com>
2025-05-09 10:26:27 -0300

    fix: force Vite to include styled-components in optimizeDeps

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Corrige __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      optimizeDeps: {
-        include: ['styled-components'],
-      },
+  optimizeDeps: {
+    include: ['styled-components'],
+  },
+  build: {
+    rollupOptions: {
+      external: [],
     },
   },
 });
